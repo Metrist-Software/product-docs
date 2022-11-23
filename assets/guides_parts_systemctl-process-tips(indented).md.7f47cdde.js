69dbc365c8f2b41e1584d1ed75cc0e6897b3d30b
DavidSabine <DavidSabine@users.noreply.github.com>
2022-11-23 20:27:41 +0000

    deploy: 4de9ea209a44274f9158c8a2bae650eaaf533661

---
+++ assets/guides_parts_systemctl-process-tips(indented).md.7f47cdde.js
@@ -0,0 +1,9 @@
+import{_ as s,c as e,o as r,a as t}from"./app.d6e879c4.js";const h=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"guides/parts/systemctl-process-tips(indented).md","lastUpdated":1669235225000}'),o={name:"guides/parts/systemctl-process-tips(indented).md"},n=t("pre",null,[t("code",null,`::: tip
+
+\`journalctl --unit metrist-orchestrator\` should show a running process.
+
+\`sudo systemctl stop metrist-orchestrator\` will stop the process.
+
+\`sudo systemctl start metrist-orchestrator\` will start the process.
+:::
+`)],-1),a=[n];function c(i,d,p,l,_,m){return r(),e("div",null,a)}const f=s(o,[["render",c]]);export{h as __pageData,f as default};

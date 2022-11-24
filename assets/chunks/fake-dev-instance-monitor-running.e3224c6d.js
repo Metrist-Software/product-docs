d6bf1ede5c0d53ca282726b779008f5c2321584d
DavidSabine <DavidSabine@users.noreply.github.com>
2022-11-24 02:04:18 +0000

    deploy: fe275f5251190838a55fd8402c24011f52941f57

---
+++ assets/chunks/fake-dev-instance-monitor-running.e3224c6d.js
@@ -0,0 +1,1 @@
+const n="/assets/fake-dev-instance-monitor-running.7fe8cb7f.png";export{n as _};
--- assets/guides_parts_systemctl-process-tips(indented).md.5fa9f831.js
+++
@@ -1,9 +0,0 @@
-import{_ as s,c as e,o as r,a as t}from"./app.37ce945b.js";const h=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"guides/parts/systemctl-process-tips(indented).md","lastUpdated":1669235225000}'),o={name:"guides/parts/systemctl-process-tips(indented).md"},n=t("pre",null,[t("code",null,`::: tip
-
-\`journalctl --unit metrist-orchestrator\` should show a running process.
-
-\`sudo systemctl stop metrist-orchestrator\` will stop the process.
-
-\`sudo systemctl start metrist-orchestrator\` will start the process.
-:::
-`)],-1),a=[n];function c(i,d,p,l,_,m){return r(),e("div",null,a)}const f=s(o,[["render",c]]);export{h as __pageData,f as default};

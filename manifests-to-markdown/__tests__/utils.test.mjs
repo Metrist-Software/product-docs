329b8819a1b9152830567fcf42b689fbe70f466e
David Sabine <david.sabine@metrist.io>
2022-11-26 14:06:43 -0500

    writeMarkdownDoc working

--- manifests-to-markdown/__tests__/utils.test.mjs
+++ manifests-to-markdown/__tests__/utils.test.mjs
@@ -6,11 +6,18 @@ import {
   vi
 } from 'vitest'
 import { config } from '../config.mjs'
-import { getSubDirectories, maybeMakeTmpDirectory } from '../src/utils.mjs'
-import { mkdtemp, readdir } from 'node:fs/promises'
+import {
+  getAllMarkdownDocs,
+  getSubDirectories,
+  maybeMakeTmpDirectory,
+  writeMarkdownDoc
+} from '../src/utils.mjs'
+import { extname } from 'node:path'
+import { mkdtemp } from 'node:fs/promises'
 
 const monitorsPath = config.__vitepressMonitorsDirectory
 const manifestsPath = config.__manifestsDirectory
+const mocksPath = `${manifestsPath.substring(0, manifestsPath.lastIndexOf(`/manifests`))}/__tests__/__mocks__`
 
 vi.mock('node:fs/promises', async () => {
   return {
@@ -55,30 +62,60 @@ describe(`maybeMakeTmpDirectory`, () => {
 
 describe(`getAllDirectories`, () => {
 
-  afterEach(() => {
-    vi.resetAllMocks()
-    vi.restoreAllMocks()
-  })
-
   it.concurrent(`throws an error if directory doesn't exist`, async () => {
     await expect(getSubDirectories(`nonexistent path`)).rejects.toThrowError(`Directory does not exist: nonexistent path`)
   })
 
   it.concurrent(`returns empty array if directory is empty`, async () => {
-    const result = await getSubDirectories(`${manifestsPath.substring(0, manifestsPath.lastIndexOf(`/manifests`))}/__tests__/__mocks__/a directory`)
+    const result = await getSubDirectories(`${mocksPath}/a directory`)
     expect(Array.isArray(result)).toBe(true)
     expect(result.length).toBe(0)
   })
 
   it(`returns an array of dirent objects`, async () => {
-    const mockPath = `${manifestsPath.substring(0, manifestsPath.lastIndexOf(`/manifests`))}/__tests__/__mocks__`
-    const result = await getSubDirectories(mockPath)
+    const result = await getSubDirectories(mocksPath)
     expect(Array.isArray(result)).toBe(true)
     expect(result.length).toBeGreaterThan(0)
     expect(result[0].isDirectory()).toBe(true)
     result.forEach((dir) => {
       expect(dir.isDirectory()).toBe(true)
     })
+  })
+
+})
+
+describe(`getAllMarkdownDocs`, () => {
+
+  it.concurrent(`returns empty array if directory has no *.md files`, async () => {
+    const result = await getAllMarkdownDocs(mocksPath)
+    expect(Array.isArray(result)).toBe(true)
+    expect(result.length).toBe(0)
+  })
+
+  it(`returns an array of .md files`, async () => {
+    const result = await getAllMarkdownDocs(`${mocksPath}/a directory`)
+    expect(Array.isArray(result)).toBe(true)
+    expect(result.length).toBeGreaterThan(0)
+    expect(result[0].isFile()).toBe(true)
+    result.forEach((file) => {
+      expect(extname(file.name)).toBe(`.md`)
+    })
+  })
+
+})
+
+describe(`writeMarkdownDoc`, () => {
+
+  it.concurrent(`writes a file`, async () => {
+    const result = await writeMarkdownDoc(
+      `${mocksPath}/tmp_directory/new.md`,
+      `mock data`
+    )
+    expect(result).toBe(undefined)
+  })
+
+  it.concurrent(`throws error if directory doesn't exist`, async () => {
+    await expect(writeMarkdownDoc(`${mocksPath}/nonexistent path/new.md`, `mock data`)).rejects.toThrowError(`Could not write file. Directory does not exist: ${mocksPath}/nonexistent path/new.md`)
   })
 
 })
--- manifests-to-markdown/src/utils.mjs
+++ manifests-to-markdown/src/utils.mjs
@@ -1,4 +1,20 @@
-import { mkdtemp, readdir } from 'node:fs/promises'
+import { extname } from 'node:path'
+import {
+  mkdtemp,
+  readdir,
+  writeFile
+} from 'node:fs/promises'
+
+export const getAllMarkdownDocs = async (path) => {
+  try {
+    const contents = await readdir(path, { withFileTypes: true })
+    return contents.filter((item) => {
+      return item.isFile() && extname(item.name) === `.md`
+    })
+  } catch (_err) {
+    throw new Error(`Could not retrieve .md files: ${path}`)
+  }
+}
 
 export const getSubDirectories = async (path) => {
   try {
@@ -6,7 +22,7 @@ export const getSubDirectories = async (path) => {
     return contents.filter((item) => {
       return item.isDirectory()
     })
-  } catch (err) {
+  } catch (_err) {
     throw new Error(`Directory does not exist: ${path}`)
   }
 }
@@ -14,17 +30,26 @@ export const getSubDirectories = async (path) => {
 export const maybeMakeTmpDirectory = async (tmpPath) => {
   try {
     return await mkdtemp(tmpPath)
-  } catch (err) {
+  } catch (_err) {
     return tmpPath
   }
 }
 
-// iterate through folders
+export const writeMarkdownDoc = async (path, data) => {
+  try {
+    return await writeFile(path, data)
+  } catch (_err) {
+    throw new Error(`Could not write file. Directory does not exist: ${path}`)
+  }
+}
+
+/*
+get folders in manifests folder
+|> for each
+    -> look for <monitor-logical-name>.md and emit as is to <foldername: author>-<monitor-logical-name>.md
+    -> look for <monitor-logicalname>.json and produce .md from template then emit...
+
+make smarter
 // check the timestamp of manifest
-// grab recent manifest
-// get the template
-// parse the manifest into the template
-// write the file to new location
-// get the index template
-// write the index.md
-// copy the index.md
+// adjust index.md also
+**/
